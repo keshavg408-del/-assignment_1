4927a4a593342b45cf8742f4ab24e2917e5362ee
keshavg408-del <keshavg408@gmail.com>
2026-02-27 15:06:59 +0530

    Add files via upload

---
+++ tt_as1.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print(f"Addition: {a+b}")
+print(f"Subtraction: {a-b}")
+print(f"Multiplication: {a*b}")
+print(f"Divison: {a/b}")
